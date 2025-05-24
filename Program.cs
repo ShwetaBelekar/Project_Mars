b0c8b8e1307788a4d7dc24c41c168c872592e9e5
Shweta Belekar <belekarshwetagmail.com>
2025-05-25 10:55:51 +1200

    Built create,edit,delete automated testcase for Skills feature

--- Program.cs
+++ Program.cs
@@ -41,80 +41,157 @@ public class Program
         IWebElement profileTab = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
         profileTab.Click();
 
-        IWebElement languageOption = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]"));
-        languageOption.Click();
+        //IWebElement languageOption = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[1]"));
+        //languageOption.Click();
 
-        IWebElement addNewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div"));
-        addNewButton.Click();
+        //IWebElement addNewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div"));
+        //addNewButton.Click();
+        //Thread.Sleep(2000);
+
+        //IWebElement addLanguageTextbox = driver.FindElement(By.XPath("//input[@placeholder='Add Language']"));
+        //addLanguageTextbox.SendKeys("English");
+
+        //IWebElement chooseLanguageLevelDropDown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select"));
+        //chooseLanguageLevelDropDown.Click();
+        //Thread.Sleep(2000);
+
+        //IWebElement basicOption = driver.FindElement(By.XPath("//option[@value='Basic']"));
+        //basicOption.Click();
+
+        //IWebElement addButton = driver.FindElement(By.XPath("//input[@value='Add']"));
+        //addButton.Click();
+
+        //IWebElement newLanguage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+
+        //if (newLanguage.Text == "English")
+        //{
+        //    Console.WriteLine("New Language record created Successfully!");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("New Language record has not been created!");
+        //}
+
+        //Thread.Sleep(2000);
+
+        //IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[3]/tr/td[3]/span[1]/i"));
+        //editButton.Click();
+
+        //IWebElement dropDownButton = driver.FindElement(By.XPath("//select[@class='ui dropdown']"));
+        //dropDownButton.Click();
+
+        //IWebElement fluentOption = driver.FindElement(By.XPath("//option[@value='Fluent']"));
+        //fluentOption.Click();
+
+        //IWebElement updateButton = driver.FindElement(By.XPath("//input[@value='Update']"));
+        //updateButton.Click();
+        //Thread.Sleep(2000);
+
+        //IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+
+        //if (newLevel.Text == "Fluent")
+        //{
+        //    Console.WriteLine("Fluent level is updated Successfully!");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Fluent level is not updated Successfully!");
+        //}
+        //Thread.Sleep(2000);
+        //IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[3]/tr/td[3]/span[2]"));
+        //deleteButton.Click();
+        //driver.Navigate().Refresh();
+        //Thread.Sleep(5000);
+
+        //IWebElement language = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+
+        //if(language.Text == "English")
+        //{
+        //    Console.WriteLine("English is present");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("English is not present");
+        //}
+
+        //driver.Quit();
+
+        IWebElement skillsOption = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]"));
+        skillsOption.Click();
         Thread.Sleep(2000);
 
-        IWebElement addLanguageTextbox = driver.FindElement(By.XPath("//input[@placeholder='Add Language']"));
-        addLanguageTextbox.SendKeys("English");
+        IWebElement aaddNewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+        aaddNewButton.Click();
 
-        IWebElement chooseLanguageLevelDropDown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select"));
-        chooseLanguageLevelDropDown.Click();
+        IWebElement addSkillTextbox = driver.FindElement(By.XPath("//input[@placeholder='Add Skill']"));
+        addSkillTextbox.SendKeys("Singing");
+
+        IWebElement chooseSkillLevelDropdown = driver.FindElement(By.XPath("//select[@name='level']"));
+        chooseSkillLevelDropdown.Click();
+        Thread.Sleep(2000);
+        IWebElement beginnerOption = driver.FindElement(By.XPath("//option[@value='Beginner']"));
+        beginnerOption.Click();
+
+        IWebElement adddButton = driver.FindElement(By.XPath("//input[@value='Add']"));
+        adddButton.Click();
         Thread.Sleep(2000);
 
-        IWebElement basicOption = driver.FindElement(By.XPath("//option[@value='Basic']"));
-        basicOption.Click();
+        IWebElement newSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
 
-        IWebElement addButton = driver.FindElement(By.XPath("//input[@value='Add']"));
-        addButton.Click();
-
-        IWebElement newLanguage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
-
-        if (newLanguage.Text == "English")
+        if (newSkill.Text == "Singing")
         {
-            Console.WriteLine("New Language record created Successfully!");
+            Console.WriteLine("Singing skill is created successfully!");
         }
         else
         {
-            Console.WriteLine("New Language record has not been created!");
+            Console.WriteLine("Singing skill is not created successfully!");
         }
 
+        IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[10]/tr/td[3]/span[1]"));
+        editButton.Click();
         Thread.Sleep(2000);
 
-        IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[3]/tr/td[3]/span[1]/i"));
-        editButton.Click();
+        IWebElement chooseeSkillLevelDropdown = driver.FindElement(By.XPath("//select[@name='level']"));
+        chooseeSkillLevelDropdown.Click();
 
-        IWebElement dropDownButton = driver.FindElement(By.XPath("//select[@class='ui dropdown']"));
-        dropDownButton.Click();
-
-        IWebElement fluentOption = driver.FindElement(By.XPath("//option[@value='Fluent']"));
-        fluentOption.Click();
+        IWebElement intermediateOption = driver.FindElement(By.XPath("//option[@value='Intermediate']"));
+        intermediateOption.Click();
 
         IWebElement updateButton = driver.FindElement(By.XPath("//input[@value='Update']"));
         updateButton.Click();
         Thread.Sleep(2000);
 
-        IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+        IWebElement newLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
 
-        if (newLevel.Text == "Fluent")
+        if (newLevel.Text == "Intermediate")
         {
-            Console.WriteLine("Fluent level is updated Successfully!");
+            Console.WriteLine("Intermediate is updated!");
         }
         else
         {
-            Console.WriteLine("Fluent level is not updated Successfully!");
+            Console.WriteLine("Intermediate is not updated!");
         }
-        Thread.Sleep(2000);
-        IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[3]/tr/td[3]/span[2]"));
+
+        IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[10]/tr/td[3]/span[2]/i"));
         deleteButton.Click();
-        driver.Navigate().Refresh();
         Thread.Sleep(5000);
 
-        IWebElement language = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+        IWebElement skills = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
 
-        if(language.Text == "English")
+        if(skills.Text == "Singing")
         {
-            Console.WriteLine("English is present");
+            Console.WriteLine("Singing is present!");
         }
         else
         {
-            Console.WriteLine("English is not present");
+            Console.WriteLine("Singing is not present!");
         }
 
-        driver.Quit();
+
+
+        
+
+
 
 
 
