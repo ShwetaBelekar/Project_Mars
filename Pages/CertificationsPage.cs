28f89f67c4a119bcbe71cdef35fa6c04f0d24934
Shweta Belekar <belekarshwetagmail.com>
2025-07-28 13:00:07 +1200

    Automated tests for certification feature

--- Pages/CertificationsPage.cs
+++ Pages/CertificationsPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,44 @@ namespace Project_Mars.Pages
 {
     public class CertificationsPage
     {
+        public void CreateCertificationRecord(IWebDriver driver)
+        {
+            IWebElement addNewButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/thead/tr/th[4]/div"));
+            addNewButton.Click();
 
+            IWebElement certificationOrAwardTextbox = driver.FindElement(By.XPath("//input[@placeholder='Certificate or Award']"));
+            certificationOrAwardTextbox.Click();
+            certificationOrAwardTextbox.SendKeys("");
+
+            IWebElement certifiedFromTextbox = driver.FindElement(By.XPath("//input[@name='certificationFrom']"));
+            certifiedFromTextbox.Click();
+            certifiedFromTextbox.SendKeys("");
+
+            IWebElement yearDropdownButton = driver.FindElement(By.XPath(""));
+            yearDropdownButton.Click();
+            yearDropdownButton.SendKeys("");
+
+            IWebElement addButton = driver.FindElement(By.XPath("//input[@value='Add']"));
+            addButton.Click();
+
+            IWebElement cancelButton = driver.FindElement(By.XPath("//input[@value='Cancel']"));
+            cancelButton.Click();
+
+
+
+
+        }
+        public void EditCertificationRecord(IWebDriver driver)
+        {
+            IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/tbody[last()]/tr/td[4]/span[1]/i"));
+            editButton.Click();
+
+
+
+
+
+
+
+        }
     }
 }
